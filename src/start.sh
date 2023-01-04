085bf3a48905aeba47a08322d5eab997cef35753
Damian Hog <56997492+hogdamian@users.noreply.github.com>
2023-01-04 22:59:39 +0100

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,110 +1,17 @@
 #!/bin/bash
-echo "Starting Script."
-# infinite loop
-for (( ; ; ))
-do
-stunde=$(date +%X)
-echo $stunde
-if [ "$stunde" = "00:00:00" ]; then
-	echo "It is  00:00 o'clock"
-	echo "Starting Speedtest."
-	sh ./script.sh
-elif [ "$stunde" = "01:00:00" ]; then
-	echo "It is  01:00 o'clock"
- 	echo "Starting Speedtest."
-	sh ./script.sh
-elif [ "$stunde" = "02:00:00" ]; then
-        echo "It is  02:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "03:00:00" ]; then
-        echo "It is  03:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "04:00:00" ]; then
-        echo "It is  04:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "05:00:00" ]; then
-        echo "It is  05:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "06:00:00" ]; then
-        echo "It is  06:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "07:00:00" ]; then
-        echo "It is  07:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "08:00:00" ]; then
-        echo "It is  08:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "09:00:00" ]; then
-        echo "It is  09:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "10:00:00" ]; then
-        echo "It is  10:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "11:00:00" ]; then
-        echo "It is  11:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "12:00:00" ]; then
-        echo "It is  12:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "13:00:00" ]; then
-        echo "It is  13:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "14:00:00" ]; then
-        echo "It is  14:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "15:16:00" ]; then
-        echo "It is  15:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "16:00:00" ]; then
-        echo "It is  16:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "17:00:00" ]; then
-        echo "It is  17:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "18:00:00" ]; then
-        echo "It is  18:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "19:00:00" ]; then
-        echo "It is  19:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "20:00:00" ]; then
-        echo "It is  20:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "21:00:00" ]; then
-        echo "It is  21:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "22:00:00" ]; then
-        echo "It is  21:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "23:00:00" ]; then
-        echo "It is  23:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
-elif [ "$stunde" = "24:00:00" ]; then
-        echo "It is  24:00 o'clock"
-        echo "Starting Speedtest."
-        sh ./script.sh
+
+# check if cron is installed
+if ! command -v cron > /dev/null; then
+  echo "Error: cron is not installed. Please install cron and try again."
+  exit 1
 fi
-sleep 1
-done
+
+# check if script.sh exists
+if ! [ -f "./script.sh" ]; then
+  echo "Error: script.sh not found. Please make sure the script is in the same directory as this script."
+  exit 1
+fi
+
+# add a cron job to run script.sh every hour
+crontab -l | { cat; echo "0 * * * * /bin/bash $(pwd)/script.sh"; } | crontab -
+echo "Successfully added cron job to run script.sh every hour."
